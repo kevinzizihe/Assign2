dd8ec5d4602317b5f86d1c61dd2c13e65c51b05a
kevinzizihe <kzi@sfu.ca>
2021-02-15 01:48:02 -0800

    Initial commit

---
+++ Assets/Scripts/HideObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObj : MonoBehaviour {
+
+
+    public GameObject item;
+    private void OnTriggerEnter(Collider other)
+    {
+        item.SetActive(false);
+    }
+}
